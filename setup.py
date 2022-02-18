ba28263d339e383acdf46ca56da954fd3b07e6df
Hayk Manukyan <s6hamanu@uni-bonn.de>
2022-02-18 17:01:07 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GLPlot',
+      version='0.1.0',
+      description='An OpenGL based interactive plotting library for python',
+      author='Hayk Manukyan',
+      author_email='haik.manukyan@gmail.com',
+      url='https://github.com/haikmanukyan/glplot',
+      packages=['glplot'],
+     )
